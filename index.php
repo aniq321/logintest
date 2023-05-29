713b9a2d563382275eb0da83852d4b48c513cb23
aniq321 <fnaticniq@gmail.com>
2023-05-29 14:14:13 +0800

    save

--- index.php
+++ index.php
@@ -9,14 +9,14 @@
 </head>
 <body>
     <form action="login.php" method="post"> 
-        <h2>LOGIN</h2>
+        <h2>LOG-IN</h2>
         <?php if (isset($_GET['error'])) { ?>
             <p class="error"><?php echo $_GET['error']; ?></p>
         <?php } ?>
-        <label>User Name</label>
-        <input type="text" name="uname" placeholder="User Name"><br>
+        <label>UserName</label>
+        <input type="text" name="uname" placeholder="UserName"><br>
 
-        <label>User Name</label>
+        <label>Password</label>
         <input type="password" name="password" placeholder="Password"><br>
 
         <button type="submit">Login</button>
