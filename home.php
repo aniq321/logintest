14070dc1202f25eea6f6e1ea8413601cdac4c26b
aniq321 <fnaticniq@gmail.com>
2023-05-29 14:30:52 +0800

    save

--- home.php
+++ home.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
 <?php
 session_start();
 
@@ -26,3 +36,5 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
     exit();
 }
 ?>
+</body>
+</html>
