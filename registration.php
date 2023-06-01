94cd189a004addbc1adc446cc00b9aaa1b9d8e06
aniq321 <fnaticniq@gmail.com>
2023-06-01 10:14:22 +0800

    save

--- registration.php
+++ registration.php
@@ -11,11 +11,44 @@
 <body>
     <div class="container">
         <?php
-        print_r($_POST);
+        if (isset($_POST['submit'])) {
+            $fullname = $_POST["fullname"];
+            $email = $_POST["email"];
+            $password = $_POST["password"];
+            $passwordRepeat = $_POST["repeat_password"];
+            
+            $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+
+            $errors = array();
+
+            if (empty($fullname) OR empty($email) OR empty($password) OR empty($passwordRepeat)) {
+                array_push($errors,"All fields are required");
+            }
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                array_push($errors, "Email is not valid");
+            }
+            if (strlen($password)<8) {
+                array_push($errors, "Password must be at leats 8 charactes long");
+            }
+            if ($password!==$passwordRepeat) {
+                array_push($errors, "Password does not match");
+            }
+
+            if (count($errors)>0) {
+                foreach ($errors as $error) {
+                    echo "<div class='alert alert-danger'>$error</div>";
+                }
+            }else{
+                require_once "database.php";
+            }
+        }
         ?>
         <form action="registration.php" method="post">
             <div class="form-group">
-                <input type="text" class="form-control" name="uname" placeholder="UserName:">
+                <input type="text" class="form-control" name="fullname" placeholder="Full Name:">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" name="email" placeholder="Email:">
             </div>
             <div class="form-group">
                 <input type="text" class="form-control" name="password" placeholder="Password:">
